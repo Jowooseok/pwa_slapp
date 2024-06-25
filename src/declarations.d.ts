b0e6d7e19550b5d4d5ea5baa358e2401f8164186
jowooseok <whdntjrsla12@naver.com>
2024-06-25 16:52:40 +0900

    chore: Add declaration file for react-qr-scanner module

---
+++ src/declarations.d.ts
@@ -0,0 +1,15 @@
+// declarations.d.ts
+declare module "react-qr-scanner" {
+  import { Component } from "react";
+
+  interface QrScannerProps {
+    delay?: number;
+    onError?: (error: any) => void;
+    onScan?: (data: string | null) => void;
+    style?: React.CSSProperties;
+  }
+
+  class QrScanner extends Component<QrScannerProps> {}
+
+  export default QrScanner;
+}
