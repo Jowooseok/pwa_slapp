e1a7261b92d6ef8adcb6df1fd05998277907ea6e
DESKTOP-EQSGHUM\whdnt <whdntjrsla12@naver.com>
2024-09-23 22:47:56 +0900

    asdwd

--- src/entities/User/api/userApi.ts
+++ src/entities/User/api/userApi.ts
@@ -5,7 +5,6 @@ export const fetchHomeData = async () => {
   try {
     const response = await api.post('/home');
     console.log('fetchHomeData response:', response); // 디버깅을 위한 로그 추가
-    console.log('fetchHomeData response.data:', response.data); // 추가 로그
     return response.data.data;
   } catch (error: any) {
     console.error('fetchHomeData 에러:', error);
