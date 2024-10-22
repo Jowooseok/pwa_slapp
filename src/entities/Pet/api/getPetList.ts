1badecda0727873f9f6fdbd0348a1417501c4eab
JungHo-K1m <qopqop55@naver.com>
2024-10-22 12:09:28 +0900

    ngrok error

--- src/entities/Pet/api/getPetList.ts
+++ src/entities/Pet/api/getPetList.ts
@@ -35,6 +35,7 @@ async function getPetList(): Promise<any> {
         const response = await api.get('/mypets', {
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
+                'ngrok-skip-browser-warning': '69420',
             },
         });
 
