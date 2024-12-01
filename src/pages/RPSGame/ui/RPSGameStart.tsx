dfee08999ebf4e721591511654379ef054b5e57a
DESKTOP-EQSGHUM\whdnt <whdntjrsla12@naver.com>
2024-12-01 22:50:36 +0900

    가위바위보 맥시멈 수정

--- src/pages/RPSGame/ui/RPSGameStart.tsx
+++ src/pages/RPSGame/ui/RPSGameStart.tsx
@@ -30,7 +30,7 @@ const RPSGameStart: React.FC<RPSGameStartProps> = ({
     const numericValue = parseInt(value);
     if (
       value === "" ||
-      (/^\d+$/.test(value) && numericValue <= allowedBetting)
+      (/^\d+$/.test(value) && numericValue <= allowedBetting+1)
     ) {
       setBetAmount(value);
       console.log(`betAmount set to: ${value}`);
@@ -136,7 +136,7 @@ const RPSGameStart: React.FC<RPSGameStartProps> = ({
             type="number"
             value={betAmount}
             onChange={handleInputChange}
-            max={allowedBetting} // 입력값 제한
+            max={allowedBetting+1} // 입력값 제한
             className="border-2 border-[#21212f] rounded-2xl h-12 text-sm font-medium px-4 mt-4 w-[342px]"
           />
 
